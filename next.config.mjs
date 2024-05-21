261c0a0506a064c4e6e24077b501621d1f51bf36
hoonkun <herokun.user@gmail.com>
2024-05-21 11:18:09 +0900

    fix: next static site generation 의 basePath 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "",
     compiler: { styledComponents: true },
     webpack(config) {
         config.module.rules.push({
